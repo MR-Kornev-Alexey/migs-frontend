f789c8b08bcce51dbdf8626f7c9ceca2dc0bf6b9
Alex Kornev <alex@mrk.digital>
2024-09-04 16:10:25 +0300

    04 09 2024 dynamicChart fix new

--- src/components/charts/victory/area-victory-charts.tsx
+++ src/components/charts/victory/area-victory-charts.tsx
@@ -36,81 +36,87 @@ const AreaVictoryChart: React.FC<AreaVictoryChartProps> = ({ sensors }) => {
 
   return (
     <div>
-      {sensors.map((sensor, index) => (
-        <div key={index} style={{ marginBottom: '20px' }}>
-          <VictoryChart
-            height={180}
-            width={500}
-            theme={VictoryTheme.material} // Добавляем тему для стилизации
-            containerComponent={<VictoryVoronoiContainer />}
-          >
-            {/* Добавляем название внутрь графика */}
-            <VictoryLabel
-              text={`${sensor.sensorName} ${sensor.sensorLocation}`}
-              x={250}  // Позиция по оси X (центр графика)
-              y={20}   // Позиция по оси Y (внутри сверху)
-              textAnchor="middle" // Центрируем текст
-              style={{ fontSize: 9}} // Стилизация текста
-            />
+      {sensors.map((sensor, index) => {
+        // Проверяем, все ли значения y равны нулю
+        const allYAreZero = sensor.sensorData.every(dataPoint => dataPoint.y === 0);
 
-            {/* Настройка оси Y */}
-            <VictoryAxis
-              dependentAxis
-              tickFormat={(t) => `${t} мкм`} // Форматирование меток на оси Y
-              style={{
-                axis: { stroke: '#000' },
-                axisLabel: { fontSize: 5 },
-                ticks: { stroke: '#000', size: 5 },
-                tickLabels: { fontSize: 5, padding: 5, fill: '#333' }, // Увеличен шрифт и добавлен цвет для меток оси Y
-                grid: { stroke: '#e6e6e6' }, // Добавлена сетка для оси Y
-              }}
-            />
-
-            {/* Настройка оси X */}
-            <VictoryAxis
-              tickFormat={(x) => `${x}`}
-              style={{
-                axis: { stroke: '#000' },
-                ticks: { stroke: '#000', size: 4 },
-                tickLabels: {
-                  fontSize: 5,
-                  fill: '#000',
-                  angle: 270+45, // Поворот меток на 90 градусов
-                  textAnchor: 'end' // Выравнивание текста по началу
-                },
-                grid: { stroke: '#e6e6e6' }, // Добавлена сетка для оси X
-              }}
-            />
-
-            <VictoryGroup
-              color={sensor.sensorColor}
-              labels={({ datum }) =>
-                `${sensor.sensorName} ${sensor.sensorLocation}\n${datum.y} мкм`
-              }
-              labelComponent={<VictoryTooltip style={{ fontSize: 8 }} />}
-              data={sensor.sensorData}
+        return (
+          <div key={index} style={{ marginBottom: '20px' }}>
+            <VictoryChart
+              height={180}
+              width={500}
+              theme={VictoryTheme.material} // Добавляем тему для стилизации
+              containerComponent={<VictoryVoronoiContainer />}
+              domain={{ y: allYAreZero ? [0, 10] : undefined }} // Устанавливаем домен по оси Y от 0 до 10, если все y равны нулю
             >
-              <VictoryArea
+              {/* Добавляем название внутрь графика */}
+              <VictoryLabel
+                text={`${sensor.sensorName} ${sensor.sensorLocation}`}
+                x={250}  // Позиция по оси X (центр графика)
+                y={20}   // Позиция по оси Y (внутри сверху)
+                textAnchor="middle" // Центрируем текст
+                style={{ fontSize: 10 }} // Стилизация текста
+              />
+
+              {/* Настройка оси Y */}
+              <VictoryAxis
+                dependentAxis
+                tickFormat={(t) => `${t} мкм`} // Форматирование меток на оси Y
                 style={{
-                  data: { fill: sensor.sensorColor, opacity: 0.3 }, // Устанавливаем цвет и прозрачность области
+                  axis: { stroke: '#000' },
+                  axisLabel: { fontSize: 7 },
+                  ticks: { stroke: '#000', size: 5 },
+                  tickLabels: { fontSize: 7, padding: 5, fill: '#333' }, // Увеличен шрифт и добавлен цвет для меток оси Y
+                  grid: { stroke: '#e6e6e6' }, // Добавлена сетка для оси Y
                 }}
               />
-              <VictoryScatter
-                size={({ active }) => (active ? 5 : 3)}
+
+              {/* Настройка оси X */}
+              <VictoryAxis
+                tickFormat={(x) => `${x}`}
+                style={{
+                  axis: { stroke: '#000' },
+                  ticks: { stroke: '#000', size: 5 },
+                  tickLabels: {
+                    fontSize: 5,
+                    fill: '#000',
+                    angle: 270+45, // Поворот меток на 90 градусов
+                    textAnchor: 'end' // Выравнивание текста по началу
+                  },
+                  grid: { stroke: '#e6e6e6' }, // Добавлена сетка для оси X
+                }}
+              />
+
+              <VictoryGroup
+                color={sensor.sensorColor}
                 labels={({ datum }) =>
                   `${sensor.sensorName} ${sensor.sensorLocation}\n${datum.y} мкм`
                 }
-                labelComponent={
-                  <VictoryTooltip
-                    style={{ fontSize: 8 }}
-                    flyoutStyle={{ fill: 'white' }}
-                  />
-                }
-              />
-            </VictoryGroup>
-          </VictoryChart>
-        </div>
-      ))}
+                labelComponent={<VictoryTooltip style={{ fontSize: 8 }} />}
+                data={sensor.sensorData}
+              >
+                <VictoryArea
+                  style={{
+                    data: { fill: sensor.sensorColor, opacity: 0.3 }, // Устанавливаем цвет и прозрачность области
+                  }}
+                />
+                <VictoryScatter
+                  size={({ active }) => (active ? 5 : 3)}
+                  labels={({ datum }) =>
+                    `${sensor.sensorName} ${sensor.sensorLocation}\n${datum.y} мкм`
+                  }
+                  labelComponent={
+                    <VictoryTooltip
+                      style={{ fontSize: 8 }}
+                      flyoutStyle={{ fill: 'white' }}
+                    />
+                  }
+                />
+              </VictoryGroup>
+            </VictoryChart>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/charts/victory/main-victory-charts.tsx
+++ src/components/charts/victory/main-victory-charts.tsx
@@ -1,58 +1,65 @@
 import React from 'react';
-import {VictoryChart, VictoryBar, VictoryAxis,  VictoryTheme, VictoryContainer} from 'victory';
+import {VictoryChart, VictoryBar, VictoryAxis, VictoryTheme, VictoryContainer} from 'victory';
 import {type VictoryChartData} from "@/types/victory-chart-data";
 
 interface Props {
   data: VictoryChartData[];
 }
+
 function MyBarChart({data}: Props) {
-  return <VictoryChart
-    containerComponent={<VictoryContainer />}
-    theme={VictoryTheme.material}
-    width={500}
-    height={260}
-    domainPadding={{ x: 20 }}
-    padding={{ top: 20, bottom: 90, left: 30, right: 30 }}
-  >
-    <VictoryBar
-      data={data}
-      labels={({ datum }) => datum.y}
-      style={{
-        data: {
-          fill: ({ datum }) => datum.color,
-        },
-        labels: {
-          fontSize: 8,
-          fill: '#000'
-        }
-      }}
-      barWidth={20} // Установка фиксированной ширины столбцов
-    />
-    <VictoryAxis
-      tickFormat={(x) => `${x}`}
-      style={{
-        axis: { stroke: '#000' },
-        axisLabel: { fontSize: 6 },
-        ticks: { stroke: '#000', size: 5 },
-        tickLabels: {
-          fontSize: 6,
-          fill: '#000',
-          angle: 270+45, // Поворот меток на 90 градусов
-          textAnchor: 'end' // Выравнивание текста по началу
-        }
-      }}
-    />
-    <VictoryAxis
-      dependentAxis
-      tickFormat={(y) => `${y}`}
-      style={{
-        axis: { stroke: '#000' },
-        axisLabel: { fontSize: 8 },
-        ticks: { stroke: '#000', size: 5 },
-        tickLabels: { fontSize: 8, fill: '#000' }
-      }}
-    />
-  </VictoryChart>
+  // Проверка, все ли значения y равны нулю
+  const allYAreZero = data.every(datum => datum.y === 0);
+
+  return (
+    <VictoryChart
+      containerComponent={<VictoryContainer />}
+      theme={VictoryTheme.material}
+      width={500}
+      height={200}
+      domainPadding={{ x: 20 }}
+      padding={{ top: 20, bottom: 90, left: 30, right: 30 }}
+      domain={{ y: allYAreZero ? [0, 10] : undefined }} // Установка диапазона оси Y от 0 до 10, если все y равны нулю
+    >
+      <VictoryBar
+        data={data}
+        labels={({ datum }) => datum.y}
+        style={{
+          data: {
+            fill: ({ datum }) => datum.color,
+          },
+          labels: {
+            fontSize: 8,
+            fill: '#000'
+          }
+        }}
+        barWidth={20} // Установка фиксированной ширины столбцов
+      />
+      <VictoryAxis
+        tickFormat={(x) => `${x}`}
+        style={{
+          axis: { stroke: '#000' },
+          axisLabel: { fontSize: 6 },
+          ticks: { stroke: '#000', size: 5 },
+          tickLabels: {
+            fontSize: 6,
+            fill: '#000',
+            angle: 270+45, // Поворот меток на 90 градусов
+            textAnchor: 'end' // Выравнивание текста по началу
+          }
+        }}
+      />
+      <VictoryAxis
+        dependentAxis
+        tickFormat={(y) => `${y}`}
+        style={{
+          axis: { stroke: '#000' },
+          axisLabel: { fontSize: 8 },
+          ticks: { stroke: '#000', size: 5 },
+          tickLabels: { fontSize: 8, fill: '#000' }
+        }}
+      />
+    </VictoryChart>
+  );
 }
 
 export default MyBarChart;
